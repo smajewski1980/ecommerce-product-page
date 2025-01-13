69a7f0070a5bbade61371ad551afb2cd2c16d4d5
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-01-12 20:16:02 -0500

    got most lightbox functionality working

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -1,6 +1,58 @@
 const mainImage = document.querySelector(".main-img-wrapper");
 const modal = document.querySelector("dialog");
 const modalClose = document.querySelector(".btn-lightbox-close");
+const thumbs = document.querySelectorAll(".lightbox-thumb-wrapper");
+const prodImgs = document.querySelectorAll(".prod-img");
+const lightboxPrev = document.querySelector(".btn-lightbox-prev");
+const lightboxNext = document.querySelector(".btn-lightbox-next");
+
+let activeProdId = "1";
+
+function handleLightboxPrev() {
+  if (activeProdId > 1) {
+    activeProdId--;
+    removeActiveImageClass();
+    addActiveImageClass();
+    removeActiveThumbClass();
+  }
+}
+function handleLightboxNext() {
+  if (activeProdId < 4) {
+    activeProdId++;
+    removeActiveImageClass();
+    addActiveImageClass();
+    removeActiveThumbClass();
+  }
+}
+
+function removeActiveImageClass() {
+  prodImgs.forEach((img) => {
+    img.classList.remove("active-prod-img");
+  });
+}
+
+function addActiveImageClass() {
+  prodImgs.forEach((img) => {
+    if (img.dataset.prod === `prod-${activeProdId}`) {
+      img.classList.add("active-prod-img");
+    }
+  });
+}
+
+function removeActiveThumbClass() {
+  thumbs.forEach((thumb) => {
+    thumb.classList.remove("active-thumb");
+  });
+}
+
+function handleThumbClick(e) {
+  activeProdId = e.target.dataset.thumb.slice(-1);
+  const newActiveThumbElem = e.target;
+  removeActiveThumbClass();
+  newActiveThumbElem.classList.add("active-thumb");
+  removeActiveImageClass();
+  addActiveImageClass();
+}
 
 modalClose.addEventListener("click", () => {
   modal.close();
@@ -9,3 +61,10 @@ modalClose.addEventListener("click", () => {
 mainImage.addEventListener("click", () => {
   modal.showModal();
 });
+
+thumbs.forEach((thumb) => {
+  thumb.addEventListener("click", handleThumbClick);
+});
+
+lightboxPrev.addEventListener("click", handleLightboxPrev);
+lightboxNext.addEventListener("click", handleLightboxNext);
