7c151b9c6532980f7c996d96f79917c24ffe82a1
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-01-19 19:09:15 -0500

    got cart item added and cart ui state updated

--- scripts/cart.js
+++ scripts/cart.js
@@ -4,14 +4,19 @@ const itemQtyElem = document.querySelector(".item-qty");
 const btnQtyInc = document.querySelector(".qty-inc");
 const btnQtyDec = document.querySelector(".qty-dec");
 const btnAddToCart = document.querySelector(".btn-add-to-cart");
+const cartItemsWrapper = document.querySelector(".cart-items-wrapper");
+const cartEmptyText = document.querySelector(".cart-empty-text");
+const btnCheckout = document.querySelector(".btn-checkout");
 
 let itemQty = 0;
 
 const sneakersObject = {
   name: "Fall Limited Edition Sneakers",
   price: 125,
-  image: "../images/image-product-1-thumbnail.jpg",
+  image: "images/image-product-1-thumbnail.jpg",
 };
+
+const orderItems = [];
 
 class CartItem {
   constructor(cartItem) {
@@ -23,24 +28,70 @@ class CartItem {
   calculateSubtotal() {
     return this.price * this.qty;
   }
+  generateCartItemHTML({ name, price, image }) {
+    const className = name.split(" ").join("-");
+
+    return `
+      <div class="cart-item flex">
+        <img src=${image} alt="" />
+        <div class="cart-item-content-wrapper flex">
+          <div class="cart-item-text-wrapper">
+            <p class="cart-item-name">${name}</p>
+
+            <div class="price-wrapper flex">
+              <p class="unit-price">$${price}</p>
+              <p class="qty">x ${this.qty}</p>
+              <p class="subtotal">$${this.calculateSubtotal()}</p>
+            </div>
+          </div>
+          <img class="btn-remove-${className}-from-cart" onClick=${handleCartItemRemove} src="images/icon-delete.svg" alt="" />
+        </div>
+      </div>
+    `;
+  }
+  addCartItemHTML() {
+    const HTML = this.generateCartItemHTML(this);
+    cartItemsWrapper.innerHTML += HTML;
+  }
+  addItemToOrder() {
+    orderItems.push(this);
+  }
+  removeItemFromCart() {
+    if (orderItems.length === 1) {
+      console.log("its getting here");
+    }
+  }
 }
 
 function handleAddToCart() {
+  cartEmptyTextToggle();
+  cartBtnCheckoutToggle();
   const newCartItem = new CartItem(sneakersObject);
   const cartSubtotal = newCartItem.calculateSubtotal();
-  console.log(newCartItem);
-  console.log(cartSubtotal);
-  // grab cart items wrapper
-  // generate HTML and inject into cart wrapper
-  // make cart appear
+  newCartItem.addCartItemHTML();
+  handleCartToggleOpen();
+  newCartItem.addItemToOrder();
+  // make trash icon remove item from cart
+}
+
+function handleCartItemRemove() {
+  this.removeItemFromCart();
 }
 
 function showItemQty() {
   itemQtyElem.textContent = itemQty;
 }
 
-function handleCartIconClick() {
+function handleCartToggleOpen() {
   cartWrapper.classList.toggle("go-away");
+}
+
+function cartEmptyTextToggle() {
+  cartEmptyText.classList.toggle("go-away");
+}
+
+function cartBtnCheckoutToggle() {
+  btnCheckout.classList.toggle("go-away");
 }
 
 function handleQtyChange(incOrDec) {
@@ -58,7 +109,7 @@ function handleQtyChange(incOrDec) {
   }
 }
 
-cartIcon.addEventListener("click", handleCartIconClick);
+cartIcon.addEventListener("click", handleCartToggleOpen);
 
 btnQtyDec.addEventListener("click", () => {
   handleQtyChange("dec");
